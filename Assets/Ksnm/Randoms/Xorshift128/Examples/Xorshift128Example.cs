8685c7e54f15d175065711d160eb94e61ff02f49
kasanami <dev.kacha@gmail.com>
2014-10-28 03:19:20 +0900

    Xorshift128追加

---
+++ Assets/Ksnm/Randoms/Xorshift128/Examples/Xorshift128Example.cs
@@ -0,0 +1,143 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+ */
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ksnm.Randoms.Examples
+{
+    public class Xorshift128Example
+    {
+#if UNITY_EDITOR
+        /// <summary>
+        /// 0～2のシード値でテスト
+        /// </summary>
+        [UnityEditor.MenuItem("Ksnm/Examples/Xorshift128Example Test1")]
+        public static void Test1()
+        {
+            var logText = "";
+            for (int seed = 0; seed < 3; seed++)
+            {
+                var random = new Xorshift128(seed);
+                logText += "seed=" + seed + "\n";
+                for (int i = 0; i < 10; i++)
+                {
+                    var v = random.NextDouble();
+                    if (v < 0 || v >= 1)
+                        Debug.LogError("random.NextDouble()=" + v);
+                    logText += "random.NextDouble() = " + random.NextDouble() + "\n";
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    var v = random.Next();
+                    if (v < 0)
+                        Debug.LogError("random.Next()=" + v);
+                    logText += "random.Next() = " + v + "\n";
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    var v = random.Next(3);
+                    if (v < 0 || v >= 3)
+                        Debug.LogError("random.Next(3)=" + v);
+                    logText += "random.Next(3) = " + v + "\n";
+                }
+                for (int i = 0; i < 10; i++)
+                {
+                    var v = random.Next(1, 3);
+                    if (v < 1 || v >= 3)
+                        Debug.LogError("random.Next(1, 3)=" + v);
+                    logText += "random.Next(1, 3) = " + v + "\n";
+                }
+                logText += "random.NextBytes() = {\n";
+                var bytes = new byte[16];
+                random.NextBytes(bytes);
+                foreach (var item in bytes)
+                {
+                    logText += item + "\n";
+                }
+                logText += "}\n";
+            }
+            Debug.Log(logText);
+        }
+        /// <summary>
+        /// シードに負の値を指定しても絶対値が同じであれば、同じ結果になるかテスト。
+        /// </summary>
+        [UnityEditor.MenuItem("Ksnm/Examples/Xorshift128Example Test2")]
+        public static void Test2()
+        {
+            var logText = "Xorshift128\n";
+            for (int seed = 1; seed <= 5; seed++)
+            {
+                var random = new Xorshift128(seed);
+                logText += "seed=" + seed + "\t";
+                var x = random.Next();
+                logText += "random.Next() = " + x + "\n";
+                random = new Xorshift128(-seed);
+                logText += "seed=" + -seed + "\t";
+                var y = random.Next();
+                logText += "random.Next() = " + y + "\n";
+                if (x != y)
+                    Debug.LogError("x != y");
+            }
+            // System.Randomも見てみる
+            logText += "System.Random\n";
+            for (int seed = 1; seed <= 5; seed++)
+            {
+                var random = new System.Random(seed);
+                logText += "seed=" + seed + "\t";
+                var x = random.Next();
+                logText += "random.Next() = " + x + "\n";
+                random = new System.Random(-seed);
+                logText += "seed=" + -seed + "\t";
+                var y = random.Next();
+                logText += "random.Next() = " + y + "\n";
+                if (x != y)
+                    Debug.LogError("x != y");
+            }
+            Debug.Log(logText);
+        }
+        /// <summary>
+        /// 短い時間に連続でコンストラクタを呼ぶと、同じ値が生成されるかテスト。
+        /// </summary>
+        [UnityEditor.MenuItem("Ksnm/Examples/Xorshift128Example Test3")]
+        public static void Test3()
+        {
+            // 短い時間に連続でコンストラクタを呼ぶと、同じ値が生成される
+            var logText = "Xorshift128\n";
+            for (int seed = 0; seed < 3; seed++)
+            {
+                var random = new Xorshift128();
+                logText += "random.Next() = " + random.Next() + "\n";
+            }
+            // System.Randomも見てみる
+            logText += "System.Random\n";
+            for (int seed = 0; seed < 3; seed++)
+            {
+                var random = new System.Random();
+                logText += "random.Next() = " + random.Next() + "\n";
+            }
+            Debug.Log(logText);
+        }
+#endif// UNITY_EDITOR
+    }
+}
---
+++ Assets/Ksnm/Randoms/Xorshift128/Xorshift128.cs
@@ -0,0 +1,117 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+ */
+/*
+    このクラスは次のアルゴリズムを使用しています。「Xorshift RNGs」
+    「Xorshift RNGs」は、ジョージ・マーサグリアが2003年に開発しました。
+
+    This class uses the following algorithm. "Xorshift RNGs"
+    The "Xorshift RNGs", George Marsaglia was developed in 2003.
+ */
+namespace Ksnm.Randoms
+{
+    /// <summary>
+    /// Xorshift RNGsをSystem.Randomを継承して実装したクラス
+    /// </summary>
+    class Xorshift128 : System.Random
+    {
+        uint w, x, y, z;
+
+        /// <summary>
+        /// 時間に応じて決定される既定のシード値を使用し、新しいインスタンスを初期化します。
+        /// </summary>
+        public Xorshift128() : this((uint)System.DateTime.Now.Ticks) { }
+
+        /// <summary>
+        /// 指定したシード値を使用して 新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="seed">擬似乱数系列の開始値を計算するために使用する数値。負数を指定した場合、その数値の絶対値が使用されます。</param>
+        /// <exception cref="System.OverflowException">seed が System.Int32.MinValue です。これは、絶対値が計算されるときにオーバーフローの原因となります。</exception>
+        public Xorshift128(int seed) : this((uint)System.Math.Abs(seed)) { }
+
+        /// <summary>
+        /// 指定したシード値を使用して 新しいインスタンスを初期化します。
+        /// </summary>
+        /// <param name="seed">擬似乱数系列の開始値を計算するために使用する数値。</param>
+        public Xorshift128(uint seed)
+            : this(123456789, 362436069, 521288629, 88675123)
+        {
+            seed = (uint)(seed * 2685821657736338717);
+            x ^= seed;
+            y ^= seed;
+            z ^= seed;
+            w ^= seed;
+        }
+
+        /// <summary>
+        /// 指定した値を使用して 新しいインスタンスを初期化します。
+        /// </summary>
+        public Xorshift128(uint x, uint y, uint z, uint w)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+            this.w = w;
+        }
+
+        /// <summary>
+        /// 0 以上で System.Int32.MaxValue より小さい乱数を返します。
+        /// </summary>
+        /// <returns>0 以上で System.Int32.MaxValue より小さい 32 ビット符号付整数。</returns>
+        public override int Next()
+        {
+            return Next(int.MaxValue);
+        }
+
+        /// <summary>
+        /// 指定した最大値より小さい 0 以上の乱数を返します。
+        /// </summary>
+        /// <param name="maxValue">生成される乱数の排他的上限値。maxValue は 0 以上にする必要があります。</param>
+        /// <returns>0 以上で maxValue 未満の 32 ビット符号付き整数。
+        /// つまり、通常は戻り値の範囲に 0 は含まれますが、maxValue は含まれません。
+        /// ただし、maxValue が 0 の場合は、0 が返されます。
+        /// </returns>
+        /// <exception cref="System.ArgumentOutOfRangeException">maxValue が 0 未満です。</exception>
+        public override int Next(int maxValue)
+        {
+            if (maxValue < 0)
+                throw new System.ArgumentOutOfRangeException();
+            if (maxValue == 0)
+                return 0;
+            uint t = (x ^ (x << 11));
+            x = y;
+            y = z;
+            z = w;
+            w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+            return (int)(w % maxValue);
+        }
+
+        /// <summary>
+        /// 0.0 と 1.0 の間の乱数を返します。
+        /// </summary>
+        /// <returns>0.0 以上 1.0 未満の倍精度浮動小数点数。</returns>
+        protected override double Sample()
+        {
+            return Next() / (double)int.MaxValue;
+        }
+    }
+}
