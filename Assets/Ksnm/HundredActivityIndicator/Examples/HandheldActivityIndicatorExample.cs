345d1c8620ddbe4d0989a27fc56ebc80377765f0
kasanami <dev.kacha@gmail.com>
2014-10-30 09:47:42 +0900

    HundredActivityIndicator追加

---
+++ Assets/Ksnm/HundredActivityIndicator/Examples/HandheldActivityIndicatorExample.cs
@@ -0,0 +1,104 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+*/
+using UnityEngine;
+using System.Collections;
+
+namespace Ksnm.Examples
+{
+    public class HandheldActivityIndicatorExample : MonoBehaviour
+    {
+        void Start()
+        {
+#if UNITY_IPHONE
+            HandheldActivityIndicator.Style = iOSActivityIndicatorStyle.WhiteLarge;
+#elif UNITY_ANDROID
+            HandheldActivityIndicator.Style = AndroidActivityIndicatorStyle.Large;
+#endif
+        }
+        void OnGUI()
+        {
+            GUI.skin.button.fontSize = 40;
+            GUI.skin.button.wordWrap = true;
+            GUI.skin.label.fontSize = 40;
+            GUI.skin.label.wordWrap = true;
+            GUILayout.BeginArea(new Rect(0, 0, Screen.width, Screen.height));
+            {
+                GUILayout.BeginVertical();
+                {
+                    GUILayout.Label("通常のUnityEngine.Handheld.***ActivityIndicator()を使用した場合");
+                    GUILayout.BeginHorizontal();
+                    {
+                        if (GUILayout.Button("Aさんが表示"))
+                        {
+                            Handheld.StartActivityIndicator();
+                        }
+                        if (GUILayout.Button("Aさんが非表示"))
+                        {
+                            Handheld.StopActivityIndicator();
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        if (GUILayout.Button("Bさんが表示"))
+                        {
+                            Handheld.StartActivityIndicator();
+                        }
+                        if (GUILayout.Button("Bさんが非表示"))
+                        {
+                            Handheld.StopActivityIndicator();
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.Label("Ksnm.HandheldActivityIndicatorを使用した場合");
+                    GUILayout.BeginHorizontal();
+                    {
+                        if (GUILayout.Button("Aさんが表示"))
+                        {
+                            HandheldActivityIndicator.Start("A");
+                        }
+                        if (GUILayout.Button("Aさんが非表示"))
+                        {
+                            HandheldActivityIndicator.Stop("A");
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+                    GUILayout.BeginHorizontal();
+                    {
+                        if (GUILayout.Button("Bさんが表示"))
+                        {
+                            HandheldActivityIndicator.Start("B");
+                        }
+                        if (GUILayout.Button("Bさんが非表示"))
+                        {
+                            HandheldActivityIndicator.Stop("B");
+                        }
+                    }
+                    GUILayout.EndHorizontal();
+                }
+                GUILayout.EndVertical();
+            }
+            GUILayout.EndArea();
+        }
+    }
+}
---
+++ Assets/Ksnm/HundredActivityIndicator/HandheldActivityIndicator.cs
@@ -0,0 +1,91 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+*/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Ksnm
+{
+    /// <summary>
+    /// 表示したActivityIndicatorが、勝手に消されないように
+    /// キーとフラグで表示状態を管理します。
+    /// </summary>
+    public static class HandheldActivityIndicator
+    {
+#if UNITY_ANDROID || UNITY_IPHONE
+        public static Dictionary<object, bool> Flags { get; private set; }
+        /// <summary>
+        /// 所望のスタイルを設定します。
+        /// </summary>
+#if UNITY_IPHONE
+        public static iOSActivityIndicatorStyle Style { get; set; }
+#elif UNITY_ANDROID
+        public static AndroidActivityIndicatorStyle Style { get; set; }
+#endif
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        static HandheldActivityIndicator()
+        {
+            Flags = new Dictionary<object, bool>();
+        }
+        /// <summary>
+        /// 表示開始
+        /// </summary>
+        public static void Start(object key)
+        {
+#if UNITY_IPHONE
+            Handheld.SetActivityIndicatorStyle(Style);
+#elif UNITY_ANDROID
+            Handheld.SetActivityIndicatorStyle(AndroidActivityIndicatorStyle.Small);
+#endif
+            Flags[key] = true;
+            Handheld.StartActivityIndicator();
+        }
+        /// <summary>
+        /// 非表示
+        /// </summary>
+        public static void Stop(object key)
+        {
+            Flags[key] = false;
+            if (Flags.Any() == true)
+                return;// いづれか一つでもtrueなら消さない
+            Handheld.StopActivityIndicator();
+        }
+        /// <summary>
+        /// 強制的に非表示
+        /// 全てのフラグはfalseになります。
+        /// </summary>
+        public static void Stop()
+        {
+            foreach (var key in Flags.Keys)
+            {
+                Flags[key] = false;
+            }
+            Handheld.StopActivityIndicator();
+        }
+#else
+#endif// UNITY_ANDROID || UNITY_IPHONE
+    }
+}
