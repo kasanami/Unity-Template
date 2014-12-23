3d75b85a2c29bc109a3b7b16d588d7d83d0f71c5
kasanami <dev.kacha@gmail.com>
2014-12-24 00:00:43 +0900

    * Colors追加 * Colors.HsvColor追加

---
+++ Assets/Ksnm/Colors/HsvColor.cs
@@ -0,0 +1,172 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+ */
+using System;
+using UnityEngine;
+
+namespace Ksnm.Colors
+{
+    /// <summary>
+    /// HSVの色を表現
+    /// ・各要素は0.0～1.0の値
+    /// 
+    /// TPDP:UnityEngine.Color と同様に Equals等も実装していきたい
+    /// </summary>
+    public struct HsvColor
+    {
+        /// <summary>
+        /// 色相(Hue)
+        /// </summary>
+        public float h;
+
+        /// <summary>
+        /// 彩度(Saturation・Chroma)
+        /// </summary>
+        public float s;
+
+        /// <summary>
+        /// 明度(Value・Lightness・Brightness)
+        /// </summary>
+        public float v;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="h">色相</param>
+        /// <param name="s">彩度</param>
+        /// <param name="v">明度</param>
+        private HsvColor(float h, float s, float v)
+        {
+            this.h = h;
+            this.s = s;
+            this.v = v;
+        }
+
+        /// <summary>
+        /// RGBAからHSVへ変換
+        /// ・不透明度は無くなる
+        /// </summary>
+        public static HsvColor FromRgb(Color rgb)
+        {
+            float r = rgb.r;
+            float g = rgb.g;
+            float b = rgb.b;
+            float max = System.Math.Max(r, System.Math.Max(g, b));
+            float min = System.Math.Min(r, System.Math.Min(g, b));
+            float h = max - min;
+            if (h > 0.0f)
+            {
+                if (max == r)
+                {
+                    h = (g - b) / h;
+                    if (h < 0.0f)
+                    {
+                        h += 6.0f;
+                    }
+                }
+                else if (max == g)
+                {
+                    h = 2.0f + (b - r) / h;
+                }
+                else
+                {
+                    h = 4.0f + (r - g) / h;
+                }
+                h /= 6;
+            }
+            float s = (max - min);
+            if (max != 0.0f)
+                s /= max;
+            float v = max;
+            return new HsvColor(h, s, v);
+        }
+
+        /// <summary>
+        /// HSVからRGBAへ変換
+        /// ・不透明度は1になる。
+        /// </summary>
+        public static Color ToRgb(HsvColor hsv)
+        {
+            float v = hsv.v;
+            float s = hsv.s;
+            float r = v;
+            float g = v;
+            float b = v;
+            if (s > 0)
+            {
+                float h = hsv.h * 6;
+                int i = (int) h;
+                float f = h - (float) i;
+                switch (i) {
+                    default:
+                    case 0:
+                        g *= 1 - s * (1 - f);
+                        b *= 1 - s;
+                        break;
+                    case 1:
+                        r *= 1 - s * f;
+                        b *= 1 - s;
+                        break;
+                    case 2:
+                        r *= 1 - s;
+                        b *= 1 - s * (1 - f);
+                        break;
+                    case 3:
+                        r *= 1 - s;
+                        g *= 1 - s * f;
+                        break;
+                    case 4:
+                        r *= 1 - s * (1 - f);
+                        g *= 1 - s;
+                        break;
+                    case 5:
+                        g *= 1 - s;
+                        b *= 1 - s * f;
+                        break;
+                }
+            }
+            return new Color(r, g, b);
+        }
+
+        public static HsvColor black { get { return new HsvColor(0, 0, 0); } }
+        public static HsvColor blue { get { return new HsvColor(4f / 6f, 1, 1); } }
+        public static HsvColor cyan { get { return new HsvColor(3f / 6f, 1, 1); } }
+        public static HsvColor gray { get { return new HsvColor(0, 0, 0.5f); } }
+        public static HsvColor green { get { return new HsvColor(2f / 6f, 1, 1); } }
+        /// <summary>
+        /// English spelling for gray.
+        /// </summary>
+        public static HsvColor grey { get { return gray; } }
+        public static HsvColor magenta { get { return new HsvColor(5f / 6f, 1, 1); } }
+        public static HsvColor red { get { return new HsvColor(0, 1, 1); } }
+        public static HsvColor white { get { return new HsvColor(0, 0, 1); } }
+        /// <summary>
+        /// UnityEngine.Color.yellow の値が微妙なので、どうしても誤差が発生する。
+        /// </summary>
+        public static HsvColor yellow { get { return new HsvColor(0.1533865f, 0.9843137f, 1); } }
+
+        public override string ToString()
+        {
+            return "HSV(" + h + ", " + s + ", " + v + ")";
+        }
+    }
+}
---
+++ Assets/Ksnm/Colors/Test/Editor/HsvColorTest.cs
@@ -0,0 +1,81 @@
+﻿/*
+ Copyright (c) 2014 Takahiro Kasanami
+ 
+ This software is provided 'as-is', without any express or implied
+ warranty. In no event will the authors be held liable for any damages
+ arising from the use of this software.
+ 
+ Permission is granted to anyone to use this software for any purpose,
+ including commercial applications, and to alter it and redistribute it
+ freely, subject to the following restrictions:
+ 
+    1. The origin of this software must not be misrepresented; you must not
+    claim that you wrote the original software. If you use this software
+    in a product, an acknowledgment in the product documentation would be
+    appreciated but is not required.
+ 
+    2. Altered source versions must be plainly marked as such, and must not be
+    misrepresented as being the original software.
+ 
+    3. This notice may not be removed or altered from any source
+    distribution.
+ */
+using UnityEngine;
+using NUnit.Framework;
+
+namespace Ksnm.Colors
+{
+    [TestFixture]
+    public class HsvColorTest
+    {
+        [Test]
+        [TestCase(0.0f, 0.0f, 0.0f)]
+        [TestCase(0.0f, 0.0f, 0.5f)]
+        [TestCase(0.0f, 0.5f, 0.0f)]
+        [TestCase(0.0f, 0.5f, 0.5f)]
+        [TestCase(0.5f, 0.0f, 0.0f)]
+        [TestCase(0.5f, 0.0f, 0.5f)]
+        [TestCase(0.5f, 0.5f, 0.0f)]
+        [TestCase(0.5f, 0.5f, 0.5f)]
+        [TestCase(0.0f, 0.0f, 1.0f)]
+        [TestCase(0.0f, 1.0f, 0.0f)]
+        [TestCase(0.0f, 1.0f, 1.0f)]
+        [TestCase(1.0f, 0.0f, 0.0f)]
+        [TestCase(1.0f, 0.0f, 1.0f)]
+        [TestCase(1.0f, 1.0f, 0.0f)]
+        [TestCase(1.0f, 1.0f, 1.0f)]
+        public void Rgb(float r, float g, float b)
+        {
+            var rgb = new Color(r, g, b);
+            var hsv = HsvColor.FromRgb(rgb);
+            var rgb2 = HsvColor.ToRgb(hsv);
+            Assert.AreEqual(rgb, rgb2);
+            //Debug.Log(rgb.ToString() + "→" + hsv.ToString());
+        }
+
+        [Test]
+        [TestCase("black")]
+        [TestCase("blue")]
+        [TestCase("cyan")]
+        [TestCase("gray")]
+        [TestCase("green")]
+        [TestCase("grey")]
+        [TestCase("magenta")]
+        [TestCase("red")]
+        [TestCase("white")]
+        // UnityEngine.Color.yellow の値が微妙なので、どうしても誤差が発生する。
+        public void StaticColor(string name)
+        {
+            if (name == "black") Assert.AreEqual(HsvColor.FromRgb(Color.black), HsvColor.black);
+            if (name == "blue") Assert.AreEqual(HsvColor.FromRgb(Color.blue), HsvColor.blue);
+            if (name == "cyan") Assert.AreEqual(HsvColor.FromRgb(Color.cyan), HsvColor.cyan);
+            if (name == "gray") Assert.AreEqual(HsvColor.FromRgb(Color.gray), HsvColor.gray);
+            if (name == "green") Assert.AreEqual(HsvColor.FromRgb(Color.green), HsvColor.green);
+            if (name == "grey") Assert.AreEqual(HsvColor.FromRgb(Color.grey), HsvColor.grey);
+            if (name == "magenta") Assert.AreEqual(HsvColor.FromRgb(Color.magenta), HsvColor.magenta);
+            if (name == "red") Assert.AreEqual(HsvColor.FromRgb(Color.red), HsvColor.red);
+            if (name == "white") Assert.AreEqual(HsvColor.FromRgb(Color.white), HsvColor.white);
+            if (name == "yellow") Assert.AreEqual(HsvColor.FromRgb(Color.yellow), HsvColor.yellow);
+        }
+    }
+}
