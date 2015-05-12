68807ae6213cc51bc66a346852eabd1c03326ac0
kasanami <dev.kacha@gmail.com>
2015-05-13 02:25:17 +0900

    * コンパイル時に有効だったマクロをログ出力に追加

--- Assets/Ksnm/CompiledTimeStamp/Editor/CompiledTimeStamp.cs
+++ Assets/Ksnm/CompiledTimeStamp/Editor/CompiledTimeStamp.cs
@@ -1,5 +1,5 @@
 ﻿/*
- Copyright (c) 2014 Takahiro Kasanami
+ Copyright (c) 2014-2015 Takahiro Kasanami
  
  This software is provided 'as-is', without any express or implied
  warranty. In no event will the authors be held liable for any damages
@@ -34,7 +34,76 @@ namespace Ksnm
     {
         static CompiledTimeStamp()
         {
-            Debug.Log("CompiledTimeStamp " + System.DateTime.Now.ToLongTimeString());
+            var message = new System.Text.StringBuilder();
+            message.AppendLine("CompiledTimeStamp " + System.DateTime.Now.ToLongTimeString());
+            message.AppendLine("Defines:");
+#if UNITY_EDITOR
+            message.AppendLine("UNITY_EDITOR");
+#endif
+#if UNITY_EDITOR_WIN
+            message.AppendLine("UNITY_EDITOR_WIN");
+#endif
+#if UNITY_EDITOR_OSX
+            message.AppendLine("UNITY_EDITOR_OSX");
+#endif
+#if UNITY_STANDALONE_OSX
+            message.AppendLine("UNITY_STANDALONE_OSX");
+#endif
+#if UNITY_STANDALONE_WIN
+            message.AppendLine("UNITY_STANDALONE_WIN");
+#endif
+#if UNITY_STANDALONE_LINUX
+            message.AppendLine("UNITY_STANDALONE_LINUX");
+#endif
+#if UNITY_STANDALONE
+            message.AppendLine("UNITY_STANDALONE");
+#endif
+#if UNITY_WEBPLAYER
+            message.AppendLine("UNITY_WEBPLAYER");
+#endif
+#if UNITY_WII
+            message.AppendLine("UNITY_WII");
+#endif
+#if UNITY_IOS
+            message.AppendLine("UNITY_IOS");
+#endif
+#if UNITY_IPHONE
+            message.AppendLine("UNITY_IPHONE");
+#endif
+#if UNITY_ANDROID
+            message.AppendLine("UNITY_ANDROID");
+#endif
+#if UNITY_PS3
+            message.AppendLine("UNITY_PS3");
+#endif
+#if UNITY_PS4
+            message.AppendLine("UNITY_PS4");
+#endif
+#if UNITY_XBOX360
+            message.AppendLine("UNITY_XBOX360");
+#endif
+#if UNITY_XBOXONE
+            message.AppendLine("UNITY_XBOXONE");
+#endif
+#if UNITY_BLACKBERRY
+            message.AppendLine("UNITY_BLACKBERRY");
+#endif
+#if UNITY_WP8
+            message.AppendLine("UNITY_WP8");
+#endif
+#if UNITY_METRO
+            message.AppendLine("UNITY_METRO");
+#endif
+#if UNITY_WINRT
+            message.AppendLine("UNITY_WINRT");
+#endif
+#if UNITY_WEBGL
+            message.AppendLine("UNITY_WEBGL");
+#endif
+#if Ksnm_Using_UniLinq
+            message.AppendLine("Ksnm_Using_UniLinq");
+#endif
+            Debug.Log(message.ToString());
         }
     }
 }
